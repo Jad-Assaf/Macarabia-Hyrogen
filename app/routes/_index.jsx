0ee75ea5c36139103a47122092d97e5bdc2753f9
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-17 10:41:37 +0200

    Test

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,10 +1,10 @@
-import React, { Suspense, lazy, startTransition } from 'react';
-import { defer } from '@shopify/remix-oxygen';
-import { useLoaderData } from '@remix-run/react';
-import { BannerSlideshow } from '../components/BannerSlideshow';
-import { CategorySlider } from '~/components/CollectionSlider';
-import { TopProductSections } from '~/components/TopProductSections';
-import { CollectionDisplay } from '~/components/CollectionDisplay';
+import React, {Suspense, lazy, startTransition} from 'react';
+import {defer} from '@shopify/remix-oxygen';
+import {useLoaderData} from '@remix-run/react';
+import {BannerSlideshow} from '../components/BannerSlideshow';
+import {CategorySlider} from '~/components/CollectionSlider';
+import {TopProductSections} from '~/components/TopProductSections';
+import {CollectionDisplay} from '~/components/CollectionDisplay';
 import BrandSection from '~/components/BrandsSection';
 
 const cache = new Map();
@@ -12,8 +12,14 @@ const cache = new Map();
 /**
  * @type {MetaFunction}
  */
-export const meta = () => {
-  return [{ title: 'Macarabia | Home' }];
+export const meta = ({data}) => {
+  return [
+    {title: data?.title || 'Macarabia | Home'},
+    {
+      name: 'description',
+      content: data?.description || 'Default description for Macarabia.',
+    },
+  ];
 };
 
 /**
@@ -36,48 +42,66 @@ export async function loader(args) {
 
   const banners = [
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/christmas-banner.jpg?v=1733318318',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/christmas-mobile-banner.jpg?v=1733318318',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/christmas-banner.jpg?v=1733318318',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/christmas-mobile-banner.jpg?v=1733318318',
       link: '/collections/christmas-sale',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/macbook-pro-m4-banner_756f37f6-cf6d-4484-80ed-8b510a64db28.jpg?v=1731332730',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/macbook-pro-m4-mobilebanner.jpg?v=1731333133',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/macbook-pro-m4-banner_756f37f6-cf6d-4484-80ed-8b510a64db28.jpg?v=1731332730',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/macbook-pro-m4-mobilebanner.jpg?v=1731333133',
       link: '/collections/apple-macbook',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/google-pixel-banner.jpg?v=1728123476',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/google-pixel-mobilebanner.jpg?v=1728123476',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/google-pixel-banner.jpg?v=1728123476',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/google-pixel-mobilebanner.jpg?v=1728123476',
       link: '/collections/google-products',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/remarkable-pro-banner_25c8cc9c-14de-4556-9e8f-5388ebc1eb1d.jpg?v=1729676718',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/remarkable-pro-mobile-banner-1.jpg?v=1729678484',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/remarkable-pro-banner_25c8cc9c-14de-4556-9e8f-5388ebc1eb1d.jpg?v=1729676718',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/remarkable-pro-mobile-banner-1.jpg?v=1729678484',
       link: '/collections/remarkable-paper-pro',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-flip-fold-6.jpg?v=1727957859',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-flip-fold6.jpg?v=1727957858',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-flip-fold-6.jpg?v=1727957859',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-flip-fold6.jpg?v=1727957858',
       link: '/collections/samsung-mobile-phones',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-banner.jpg?v=1726322159',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-Mobile.jpg?v=1726321600',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-banner.jpg?v=1726322159',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-Mobile.jpg?v=1726321600',
       link: '/collections/apple-iphone',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-Pro_655c6ee7-a66c-4ed9-9976-99be3122e7b6.jpg?v=1726321897',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-Pro-mobile.jpg?v=1726321600',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-Pro_655c6ee7-a66c-4ed9-9976-99be3122e7b6.jpg?v=1726321897',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/iphone-16-Pro-mobile.jpg?v=1726321600',
       link: '/collections/apple-iphone',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/Garmin.jpg?v=1726321601',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/Garmin-mobile-banner.jpg?v=1726321601',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/Garmin.jpg?v=1726321601',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/Garmin-mobile-banner.jpg?v=1726321601',
       link: '/products/garmin-fenix®-8-47-mm-amoled-sapphire-premium-multisport-gps-watch',
     },
     {
-      desktopImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ipad-banner-2_a2c3f993-278f-48c1-82de-ac42ceb6f3fc.jpg?v=1716031887',
-      mobileImageUrl: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ipad_3a178a79-4428-4aac-b5bd-41ad3f04e33a.jpg?v=1716031354',
+      desktopImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ipad-banner-2_a2c3f993-278f-48c1-82de-ac42ceb6f3fc.jpg?v=1716031887',
+      mobileImageUrl:
+        'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ipad_3a178a79-4428-4aac-b5bd-41ad3f04e33a.jpg?v=1716031354',
       link: '/collections/apple-ipad',
     },
   ];
@@ -94,7 +118,7 @@ export async function loader(args) {
   };
 
   // Cache the new data
-  cache.set(cacheKey, { value: newData, expiry: now + cacheTTL });
+  cache.set(cacheKey, {value: newData, expiry: now + cacheTTL});
 
   return defer(newData, {
     headers: {
@@ -103,38 +127,53 @@ export async function loader(args) {
   });
 }
 
-async function loadCriticalData({ context }) {
+async function loadCriticalData({context}) {
+  const {storefront} = context;
   const menuHandle = 'main-menu';
-  const { menu } = await context.storefront.query(GET_MENU_QUERY, {
-    variables: { handle: menuHandle },
+  const {menu} = await context.storefront.query(GET_MENU_QUERY, {
+    variables: {handle: menuHandle},
   });
 
+  const {shop} = await storefront.query(
+    `#graphql
+      query ShopDetails {
+        shop {
+          name
+          description
+        }
+      }
+    `,
+  );
+
   if (!menu) {
-    throw new Response('Menu not found', { status: 404 });
+    throw new Response('Menu not found', {status: 404});
   }
 
   const menuHandles = menu.items.map((item) =>
-    item.title.toLowerCase().replace(/\s+/g, '-')
+    item.title.toLowerCase().replace(/\s+/g, '-'),
   );
 
-  const [sliderCollections, menuCollections, newArrivalsCollection] = await Promise.all([
-    fetchCollectionsByHandles(context, menuHandles),
-    fetchMenuCollections(context, menuHandles),
-    fetchCollectionByHandle(context, 'new-arrivals'),
-  ]);
+  const [sliderCollections, menuCollections, newArrivalsCollection] =
+    await Promise.all([
+      fetchCollectionsByHandles(context, menuHandles),
+      fetchMenuCollections(context, menuHandles),
+      fetchCollectionByHandle(context, 'new-arrivals'),
+    ]);
 
   return {
     sliderCollections,
     menuCollections,
     newArrivalsCollection,
+    title: shop.name,
+    description: shop.description,
   };
 }
 
 // Fetch a single collection by handle
 async function fetchCollectionByHandle(context, handle) {
-  const { collectionByHandle } = await context.storefront.query(
+  const {collectionByHandle} = await context.storefront.query(
     GET_COLLECTION_BY_HANDLE_QUERY,
-    { variables: { handle } }
+    {variables: {handle}},
   );
   return collectionByHandle || null;
 }
@@ -142,8 +181,8 @@ async function fetchCollectionByHandle(context, handle) {
 // Fetch menu collections
 async function fetchMenuCollections(context, menuHandles) {
   const collectionsPromises = menuHandles.map(async (handle) => {
-    const { menu } = await context.storefront.query(GET_MENU_QUERY, {
-      variables: { handle },
+    const {menu} = await context.storefront.query(GET_MENU_QUERY, {
+      variables: {handle},
     });
 
     if (!menu || !menu.items || menu.items.length === 0) {
@@ -152,9 +191,9 @@ async function fetchMenuCollections(context, menuHandles) {
 
     const collectionPromises = menu.items.map(async (item) => {
       const sanitizedHandle = item.title.toLowerCase().replace(/\s+/g, '-');
-      const { collectionByHandle } = await context.storefront.query(
+      const {collectionByHandle} = await context.storefront.query(
         GET_COLLECTION_BY_HANDLE_QUERY,
-        { variables: { handle: sanitizedHandle } }
+        {variables: {handle: sanitizedHandle}},
       );
       return collectionByHandle || null;
     });
@@ -170,9 +209,9 @@ async function fetchMenuCollections(context, menuHandles) {
 // Fetch collections by handles for sliders
 async function fetchCollectionsByHandles(context, handles) {
   const collectionPromises = handles.map(async (handle) => {
-    const { collectionByHandle } = await context.storefront.query(
+    const {collectionByHandle} = await context.storefront.query(
       GET_COLLECTION_BY_HANDLE_QUERY,
-      { variables: { handle } }
+      {variables: {handle}},
     );
     return collectionByHandle || null;
   });
@@ -182,31 +221,136 @@ async function fetchCollectionsByHandles(context, handles) {
 }
 
 const brandsData = [
-  { name: "Apple", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/apple-new.jpg?v=1733388855", link: "/collections/apple" },
-  { name: "HP", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/hp-new.jpg?v=1733388855", link: "/collections/hp-products" },
-  { name: "MSI", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/msi-new.jpg?v=1733388855", link: "/collections/msi-products" },
-  { name: "Marshall", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/marshall-new.jpg?v=1733388855", link: "/collections/marshall-collection" },
-  { name: "JBL", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/jbl-new.jpg?v=1733388856", link: "/collections/jbl-collection" },
-  { name: "Dell", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/dell-new.jpg?v=1733388855", link: "/collections/dell-products" },
-  { name: "Garmin", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/garmin-new.jpg?v=1733393801", link: "/collections/garmin-smart-watch" },
-  { name: "Asus", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/asus-new.jpg?v=1733388855", link: "/collections/asus-products" },
-  { name: "Samsung", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-new.jpg?v=1733388855", link: "/collections/samsung-products" },
-  { name: "Sony", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/sony-new.jpg?v=1733389303", link: "/collections/sony" },
-  { name: "Benq", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/benq.jpg?v=1733388855", link: "/collections/benq-products" },
-  { name: "Tp-link", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/tp-link.jpg?v=1733388855", link: "/collections/tp-link-products" },
-  { name: "Nothing", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/nothing-new.jpg?v=1733388855", link: "/collections/nothing-products" },
-  { name: "Xiaomi", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/mi-new.jpg?v=1733388855", link: "/collections/xiaomi-products" },
-  { name: "Microsoft", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/microsoft-new.jpg?v=1733388855", link: "/collections/microsoft-products" },
-  { name: "Nintendo", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/nintendo-new.jpg?v=1733388855", link: "/collections/nintendo-products" },
-  { name: "Lenovo", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/lenovo-new.jpg?v=1733388855", link: "/collections/lenovo-products" },
-  { name: "LG", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/lg-new.jpg?v=1733388855", link: "/collections/lg-products" },
-  { name: "Meta", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/meta-new.jpg?v=1733388855", link: "/collections/meta-products" },
-  { name: "Ubiquiti", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ubiquiti-new.jpg?v=1733388855", link: "/collections/ubiquiti-products" },
-  { name: "Philips", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/Philips-new.jpg?v=1733388855", link: "/collections/philips-products" },
+  {
+    name: 'Apple',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/apple-new.jpg?v=1733388855',
+    link: '/collections/apple',
+  },
+  {
+    name: 'HP',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/hp-new.jpg?v=1733388855',
+    link: '/collections/hp-products',
+  },
+  {
+    name: 'MSI',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/msi-new.jpg?v=1733388855',
+    link: '/collections/msi-products',
+  },
+  {
+    name: 'Marshall',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/marshall-new.jpg?v=1733388855',
+    link: '/collections/marshall-collection',
+  },
+  {
+    name: 'JBL',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/jbl-new.jpg?v=1733388856',
+    link: '/collections/jbl-collection',
+  },
+  {
+    name: 'Dell',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/dell-new.jpg?v=1733388855',
+    link: '/collections/dell-products',
+  },
+  {
+    name: 'Garmin',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/garmin-new.jpg?v=1733393801',
+    link: '/collections/garmin-smart-watch',
+  },
+  {
+    name: 'Asus',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/asus-new.jpg?v=1733388855',
+    link: '/collections/asus-products',
+  },
+  {
+    name: 'Samsung',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-new.jpg?v=1733388855',
+    link: '/collections/samsung-products',
+  },
+  {
+    name: 'Sony',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/sony-new.jpg?v=1733389303',
+    link: '/collections/sony',
+  },
+  {
+    name: 'Benq',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/benq.jpg?v=1733388855',
+    link: '/collections/benq-products',
+  },
+  {
+    name: 'Tp-link',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/tp-link.jpg?v=1733388855',
+    link: '/collections/tp-link-products',
+  },
+  {
+    name: 'Nothing',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/nothing-new.jpg?v=1733388855',
+    link: '/collections/nothing-products',
+  },
+  {
+    name: 'Xiaomi',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/mi-new.jpg?v=1733388855',
+    link: '/collections/xiaomi-products',
+  },
+  {
+    name: 'Microsoft',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/microsoft-new.jpg?v=1733388855',
+    link: '/collections/microsoft-products',
+  },
+  {
+    name: 'Nintendo',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/nintendo-new.jpg?v=1733388855',
+    link: '/collections/nintendo-products',
+  },
+  {
+    name: 'Lenovo',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/lenovo-new.jpg?v=1733388855',
+    link: '/collections/lenovo-products',
+  },
+  {
+    name: 'LG',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/lg-new.jpg?v=1733388855',
+    link: '/collections/lg-products',
+  },
+  {
+    name: 'Meta',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/meta-new.jpg?v=1733388855',
+    link: '/collections/meta-products',
+  },
+  {
+    name: 'Ubiquiti',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ubiquiti-new.jpg?v=1733388855',
+    link: '/collections/ubiquiti-products',
+  },
+  {
+    name: 'Philips',
+    image:
+      'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/Philips-new.jpg?v=1733388855',
+    link: '/collections/philips-products',
+  },
 ];
 
 export default function Homepage() {
-  const { banners, sliderCollections, deferredData } = useLoaderData();
+  const {banners, sliderCollections, deferredData} = useLoaderData();
 
   const menuCollections = deferredData?.menuCollections || [];
   const newArrivalsCollection = deferredData?.newArrivalsCollection;
@@ -215,9 +359,11 @@ export default function Homepage() {
     <div className="home">
       <BannerSlideshow banners={banners} />
       <CategorySlider sliderCollections={sliderCollections} />
-      {newArrivalsCollection && <TopProductSections collection={newArrivalsCollection} />}
+      {newArrivalsCollection && (
+        <TopProductSections collection={newArrivalsCollection} />
+      )}
       <CollectionDisplay menuCollections={menuCollections} />
-      <BrandSection brands={brandsData}/>
+      <BrandSection brands={brandsData} />
     </div>
   );
 }
