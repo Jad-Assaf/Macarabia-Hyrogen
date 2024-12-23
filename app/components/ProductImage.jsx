c5495a76b186d99aa63a140544ec9596ffe6fc75
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-23 11:49:41 +0200

    Test

--- app/components/ProductImage.jsx
+++ app/components/ProductImage.jsx
@@ -1,92 +1,171 @@
-import {useEffect, useState} from 'react';
-import {MediaFile} from '@shopify/hydrogen-react';
+import { useEffect, useState } from 'react';
+import { Image } from '@shopify/hydrogen';
 import Lightbox from 'yet-another-react-lightbox';
 import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import 'yet-another-react-lightbox/styles.css';
 import '../styles/ProductImage.css';
-import {useSwipeable} from 'react-swipeable';
+import { useSwipeable } from 'react-swipeable';
 
-export function ProductImages({media, selectedVariantMedia}) {
+const LeftArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <polyline points="15 18 9 12 15 6"></polyline>
+  </svg>
+);
+
+const RightArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <polyline points="9 18 15 12 9 6"></polyline>
+  </svg>
+);
+
+/**
+ * @param {{
+ *   images: Array<{node: ProductFragment['images']['edges'][0]['node']}>;
+ * }}
+ */
+export function ProductImages({ images, selectedVariantImage }) {
   const [isLightboxOpen, setIsLightboxOpen] = useState(false);
-  const [selectedMediaIndex, setSelectedMediaIndex] = useState(0);
-  const [mediaKey, setMediaKey] = useState(0);
-  const [isMediaLoaded, setIsMediaLoaded] = useState(false);
-
-  const selectedMedia = media[selectedMediaIndex]?.node;
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+  const [imageKey, setImageKey] = useState(0);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [isVariantSelected, setIsVariantSelected] = useState(false);
 
   useEffect(() => {
-    setMediaKey((prev) => prev + 1);
-    setIsMediaLoaded(false);
-  }, [selectedMediaIndex]);
+    if (selectedVariantImage) {
+      const variantImageIndex = images.findIndex(({ node }) => node.id === selectedVariantImage.id);
+      if (variantImageIndex >= 0 && !isVariantSelected) {
+        setSelectedImageIndex(variantImageIndex);
+        setIsVariantSelected(true);
+      }
+    }
+  }, [selectedVariantImage, images, isVariantSelected]);
 
-  const handlePrevMedia = () => {
-    setSelectedMediaIndex((prevIndex) =>
-      prevIndex === 0 ? media.length - 1 : prevIndex - 1,
+  useEffect(() => {
+    setIsVariantSelected(false);
+  }, [selectedVariantImage]);
+
+  const selectedImage = images[selectedImageIndex]?.node;
+
+  useEffect(() => {
+    setImageKey((prevKey) => prevKey + 1);
+    setIsImageLoaded(false);
+  }, [selectedImageIndex]);
+
+  const handlePrevImage = () => {
+    setSelectedImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
+    setIsVariantSelected(false);
   };
 
-  const handleNextMedia = () => {
-    setSelectedMediaIndex((prevIndex) =>
-      prevIndex === media.length - 1 ? 0 : prevIndex + 1,
+  const handleNextImage = () => {
+    setSelectedImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
     );
+    setIsVariantSelected(false);
   };
 
+  // Swipe Handlers
   const swipeHandlers = useSwipeable({
-    onSwipedLeft: handleNextMedia,
-    onSwipedRight: handlePrevMedia,
+    onSwipedLeft: handleNextImage,
+    onSwipedRight: handlePrevImage,
+    trackMouse: true, // Allows swiping with a mouse for desktops
   });
 
   return (
-    <div className="product-media-container">
-      <div className="thumbnails">
-        {media.map(({node: mediaItem}, index) => (
-          <div
-            key={mediaItem.id}
-            className={`thumbnail ${
-              index === selectedMediaIndex ? 'active' : ''
-            }`}
-            onClick={() => setSelectedMediaIndex(index)}
-          >
-            <MediaFile
-              data={mediaItem}
-              options={{
-                width: 100,
-                height: 100,
-                crop: 'center',
-              }}
-            />
-          </div>
-        ))}
+    <div className="product-images-container">
+      <div className="thumbContainer">
+        <div className="thumbnails">
+          {images.map(({ node: image }, index) => (
+            <div
+              key={image.id}
+              className={`thumbnail ${index === selectedImageIndex ? 'active' : ''}`}
+              onClick={() => setSelectedImageIndex(index)}
+            >
+              <Image
+                data={image}
+                alt={image.altText || 'Thumbnail Image'}
+                aspectRatio="1/1"
+                width={100}
+                height={100}
+                loading="lazy"
+              />
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div className="main-media" {...swipeHandlers}>
-        {selectedMedia && (
+      <div
+        className="main-image"
+        onClick={() => setIsLightboxOpen(true)}
+        style={{ cursor: 'grab' }}
+        {...swipeHandlers} // Attach swipe handlers to the main image
+      >
+        {selectedImage && (
           <motion.div
-            initial={{opacity: 0}}
-            animate={{opacity: isMediaLoaded ? 1 : 0}}
-            transition={{duration: 0.3}}
+            initial={{ filter: 'blur(10px)' }}
+            animate={{ filter: isImageLoaded ? 'blur(0px)' : 'blur(10px)' }}
+            transition={{ duration: 0.3 }}
           >
-            <MediaFile
-              key={mediaKey}
-              data={selectedMedia}
-              options={{
-                width: 600,
-                height: 600,
-              }}
-              onLoad={() => setIsMediaLoaded(true)}
+            <Image
+              key={imageKey}
+              data={selectedImage}
+              alt={selectedImage.altText || 'Product Image'}
+              aspectRatio="1/1"
+              sizes="(min-width: 45em) 50vw, 100vw"
+              width="570px"
+              height="570px"
+              loading="eager"
+              onLoad={() => setIsImageLoaded(true)}
             />
           </motion.div>
         )}
+        <div className="ImageArrows">
+          <button
+            className="prev-button"
+            onClick={(e) => {
+              e.stopPropagation();
+              handlePrevImage();
+            }}
+          >
+            <LeftArrowIcon />
+          </button>
+          <button
+            className="next-button"
+            onClick={(e) => {
+              e.stopPropagation();
+              handleNextImage();
+            }}
+          >
+            <RightArrowIcon />
+          </button>
+        </div>
       </div>
 
       {isLightboxOpen && (
         <Lightbox
           open={isLightboxOpen}
           close={() => setIsLightboxOpen(false)}
-          index={selectedMediaIndex}
-          slides={media.map(({node}) => ({src: node.previewImage?.url || ''}))}
-          onIndexChange={setSelectedMediaIndex}
+          index={selectedImageIndex}
+          slides={images.map(({ node }) => ({ src: node.url }))}
+          onIndexChange={setSelectedImageIndex}
           plugins={[Fullscreen]}
         />
       )}
