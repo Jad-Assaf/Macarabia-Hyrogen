f1d2559046978fdaf93be5ad285cf73859fb0895
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-26 12:12:55 +0200

    Test

--- app/components/CollectionRows.jsx
+++ app/components/CollectionRows.jsx
@@ -42,7 +42,7 @@ const CollectionRows = ({menuCollections}) => {
                   ref={ref}
                   style={{
                     opacity: inView ? 1 : 0,
-                    transition: `opacity 0.3s ease ${collectionIndex * 0.1}s`,
+                    transition: `opacity 0.5s ease ${collectionIndex * 0.1}s`,
                   }}
                 >
                   <CollectionItem
@@ -75,7 +75,7 @@ const CollectionRows = ({menuCollections}) => {
                   ref={productRowRef}
                   style={{
                     opacity: productRowInView ? 1 : 0,
-                    transition: 'opacity 0.3s ease',
+                    transition: 'opacity 0.5s ease',
                   }}
                 >
                   {productRowInView && (
