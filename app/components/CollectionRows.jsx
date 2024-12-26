03039d3e89389a81a7697ff97054330b6a070de9
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-26 11:00:39 +0200

    Test

--- app/components/CollectionRows.jsx
+++ app/components/CollectionRows.jsx
@@ -149,32 +149,4 @@ const CollectionItem = ({collection, delay}) => {
   );
 };
 
-const LeftArrowIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <polyline points="15 18 9 12 15 6"></polyline>
-  </svg>
-);
-
-const RightArrowIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <polyline points="9 18 15 12 9 6"></polyline>
-  </svg>
-);
-
 export default CollectionRows;
