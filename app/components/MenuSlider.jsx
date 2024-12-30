455d33f0a84add623502f6cfe220c185bf9055eb
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-30 12:39:04 +0200

    Test

--- app/components/MenuSlider.jsx
+++ app/components/MenuSlider.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
+import {Link} from 'react-router-dom';
 import {useInView} from 'react-intersection-observer';
-import {CollectionItem} from './CollectionRows';
 
 /**
  * MenuSlider Component
@@ -24,16 +24,55 @@ const MenuSlider = ({handle, menuCollection}) => {
           return (
             <div className="animated-menu-item" ref={ref} key={collection.id}>
               {inView && (
-                <CollectionItem
-                  collection={collection}
-                  index={collectionIndex}
-                />
+                <Link
+                  to={`/collections/${collection.handle}`}
+                  className="menu-item-container"
+                >
+                  <div className="menu-item-image-wrapper">
+                    <CollectionImage collection={collection} />
+                  </div>
+                  <div className="category-title">{collection.title}</div>
+                </Link>
               )}
             </div>
           );
         })}
       </div>
     </div>
+  );
+};
+
+/**
+ * CollectionImage Component
+ * Handles image rendering and loading state.
+ */
+const CollectionImage = ({collection}) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      {isLoading && <div className="menu-item-shimmer-effect"></div>}
+      {collection.image && (
+        <img
+          src={`${collection.image.url}?width=300&quality=15`}
+          srcSet={`${collection.image.url}?width=300&quality=15 300w,
+                   ${collection.image.url}?width=600&quality=15 600w,
+                   ${collection.image.url}?width=1200&quality=15 1200w`}
+          alt={collection.image.altText || collection.title}
+          className={`menu-item-image ${
+            isLoading ? '' : 'menu-item-image-loaded'
+          }`}
+          width={150}
+          height={150}
+          loading="lazy"
+          onLoad={handleImageLoad}
+        />
+      )}
+    </>
   );
 };
 
