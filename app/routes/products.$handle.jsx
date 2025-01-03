0d0abf3ea109afd505377f5f0721ac5214642038
Jad Assaf <jad.a.assaf@gmail.com>
2025-01-03 15:00:27 +0200

    Test

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -32,8 +32,7 @@ export const meta = ({data}) => {
     text?.length > maxLength ? `${text.slice(0, maxLength - 3)}...` : text;
 
   const image =
-    currentVariant?.image?.url ||
-    product?.metafields?.customImage ||
+    product.images?.edges?.[0]?.node?.url ||
     'https://cdn.shopify.com/s/files/1/0858/6821/6639/files/macarabialogo01_303ae373-185d-40f3-8271-df151d977a10.png?v=1706447237';
 
   return getSeoMeta({
