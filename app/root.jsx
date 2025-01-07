d3d7dcbd777023a83428459c0a2fdfdba20cc79b
Jad Assaf <jad.a.assaf@gmail.com>
2025-01-07 14:42:20 +0200

    Test

--- app/root.jsx
+++ app/root.jsx
@@ -256,42 +256,36 @@ export function ErrorBoundary() {
   );
 }
 
-function loadFacebookPixel() {
-  (function (f, b, e, v, n, t, s) {
-    if (f.fbq) return;
-    n = f.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-    };
-    if (!f._fbq) f._fbq = n;
-    n.push = n;
-    n.loaded = true;
-    n.version = '2.0';
-    n.queue = [];
-    t = b.createElement(e);
-    t.async = true;
-    t.src = v;
-    s = b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t, s);
-  })(
-    window,
-    document,
-    'script',
-    'https://connect.facebook.net/en_US/fbevents.js',
-  );
-
-  fbq('init', '321309553208857'); // Replace with your Pixel ID
-  fbq('track', 'PageView'); // Track the PageView event
-}
-
 function FacebookPixel() {
   React.useEffect(() => {
     if (typeof window !== 'undefined') {
-      loadFacebookPixel();
+      // Inject the Facebook Pixel
+      (function (f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function () {
+          n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = true;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = true;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+      // Initialize Pixel
+      fbq('init', '321309553208857');
+      fbq('track', 'PageView'); // Fire the PageView event
     }
   }, []);
 
   return null;
 }
+
 
 /** @typedef {LoaderReturnData} RootLoader */
 
