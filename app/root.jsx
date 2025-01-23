1ad222cb4125a13768cc6dc02c549641c9134f94
Jad Assaf <jad.a.assaf@gmail.com>
2025-01-23 10:36:08 +0200

    Meta Pixel Update

--- app/root.jsx
+++ app/root.jsx
@@ -20,6 +20,7 @@ import {PageLayout} from '~/components/PageLayout';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 import {useEffect, useState} from 'react';
 import ClarityTracker from './components/ClarityTracker';
+import MetaPixel from './components/MetaPixel';
 
 /**
  * This is important to avoid re-fetching root queries on sub-navigations
@@ -206,31 +207,7 @@ export function Layout({children}) {
             `,
           }}
         ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '321309553208857');
-            fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=321309553208857&ev=PageView&noscript=1"
-            alt="Meta Pixel"
-          />
-        </noscript>
+        <MetaPixel />
       </head>
       <body>
         <ClarityTracker clarityId={clarityId} />
