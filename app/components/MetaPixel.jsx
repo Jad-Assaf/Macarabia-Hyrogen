b18bb32d52bfc895a47abbcbafb583dc3c170d44
Jad Assaf <jad.a.assaf@gmail.com>
2025-01-23 16:17:01 +0200

    Meta Pixel Update

--- app/components/MetaPixel.jsx
+++ app/components/MetaPixel.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
 export default function MetaPixel() {
   useEffect(() => {
@@ -27,91 +27,99 @@ export default function MetaPixel() {
       'https://connect.facebook.net/en_US/fbevents.js',
     );
 
-    // Initialize Meta Pixel with your ID
-    fbq('init', '321309553208857');
-    fbq('track', 'PageView');
+    if (window.fbq) {
+      // Initialize Meta Pixel with your ID
+      fbq('init', '321309553208857');
+      fbq('track', 'PageView');
 
-    if (window.analytics) {
-      // Product Events
-      window.analytics.subscribe('product_viewed', async (event) => {
-        fbq('track', 'ViewContent', {
-          content_ids: [event.data?.productVariant?.id],
-          content_name: event.data?.productVariant?.title,
-          currency: event.data?.productVariant?.price?.currencyCode,
-          value: event.data?.productVariant?.price?.amount,
-        });
-      });
-
-      window.analytics.subscribe('product_added_to_cart', async (event) => {
-        fbq('track', 'AddToCart', {
-          content_ids: [event.data?.cartLine?.merchandise?.productVariant?.id],
-          content_name:
-            event.data?.cartLine?.merchandise?.productVariant?.title,
-          currency:
-            event.data?.cartLine?.merchandise?.productVariant?.price
-              ?.currencyCode,
-          value:
-            event.data?.cartLine?.merchandise?.productVariant?.price?.amount,
-          content_type: 'product',
-        });
-      });
-
-      window.analytics.subscribe('product_removed_from_cart', async (event) => {
-        fbq('track', 'RemoveFromCart', {
-          content_ids: [event.data?.cartLine?.merchandise?.productVariant?.id],
-          content_name:
-            event.data?.cartLine?.merchandise?.productVariant?.title,
-          currency:
-            event.data?.cartLine?.merchandise?.productVariant?.price
-              ?.currencyCode,
-          value:
-            event.data?.cartLine?.merchandise?.productVariant?.price?.amount,
-          content_type: 'product',
-        });
-      });
-
-      window.analytics.subscribe('purchase_completed', async (event) => {
-        fbq('track', 'Purchase', {
-          content_ids: event.data?.lineItems?.map(
-            (item) => item.productVariant.id,
-          ),
-          value: event.data?.totalPrice?.amount,
-          currency: event.data?.totalPrice?.currencyCode,
-          content_type: 'product',
-        });
-      });
-
-      window.analytics.subscribe('checkout_started', async (event) => {
-        fbq('track', 'InitiateCheckout'); 
-      });
-
-      window.analytics.subscribe('checkout_completed', async (event) => {
-        fbq('track', 'CompleteRegistration'); 
-      });
-
-      // Other Events
-      window.analytics.subscribe('page_view', async (event) => {
-        fbq('track', 'PageView'); 
-      });
-
-      window.analytics.subscribe('lead', async (event) => {
-        fbq('track', 'Lead'); 
-      });
-
-      window.analytics.subscribe('custom_event', async (event) => {
-        // Handle custom events based on event.data
-        // Example:
-        if (event.data.eventName === 'Search') {
-          fbq('track', 'Search', {
-            content_ids: [event.data.searchQuery], 
+      if (window.analytics) {
+        // Product Events
+        window.analytics.subscribe('product_viewed', (event) => {
+          fbq('track', 'ViewContent', {
+            content_ids: [event.data?.productVariant?.id],
+            content_name: event.data?.productVariant?.title,
+            currency: event.data?.productVariant?.price?.currencyCode,
+            value: event.data?.productVariant?.price?.amount,
           });
-        }
-      });
+        });
+
+        window.analytics.subscribe('product_added_to_cart', (event) => {
+          fbq('track', 'AddToCart', {
+            content_ids: [
+              event.data?.cartLine?.merchandise?.productVariant?.id,
+            ],
+            content_name:
+              event.data?.cartLine?.merchandise?.productVariant?.title,
+            currency:
+              event.data?.cartLine?.merchandise?.productVariant?.price
+                ?.currencyCode,
+            value:
+              event.data?.cartLine?.merchandise?.productVariant?.price?.amount,
+            content_type: 'product',
+          });
+        });
+
+        window.analytics.subscribe('product_removed_from_cart', (event) => {
+          fbq('track', 'RemoveFromCart', {
+            content_ids: [
+              event.data?.cartLine?.merchandise?.productVariant?.id,
+            ],
+            content_name:
+              event.data?.cartLine?.merchandise?.productVariant?.title,
+            currency:
+              event.data?.cartLine?.merchandise?.productVariant?.price
+                ?.currencyCode,
+            value:
+              event.data?.cartLine?.merchandise?.productVariant?.price?.amount,
+            content_type: 'product',
+          });
+        });
+
+        window.analytics.subscribe('purchase_completed', (event) => {
+          fbq('track', 'Purchase', {
+            content_ids: event.data?.lineItems?.map(
+              (item) => item.productVariant.id,
+            ),
+            value: event.data?.totalPrice?.amount,
+            currency: event.data?.totalPrice?.currencyCode,
+            content_type: 'product',
+          });
+        });
+
+        window.analytics.subscribe('checkout_started', () => {
+          fbq('track', 'InitiateCheckout');
+        });
+
+        window.analytics.subscribe('checkout_completed', () => {
+          fbq('track', 'CompleteRegistration');
+        });
+
+        // Other Events
+        window.analytics.subscribe('page_view', () => {
+          fbq('track', 'PageView');
+        });
+
+        window.analytics.subscribe('lead', () => {
+          fbq('track', 'Lead');
+        });
+
+        window.analytics.subscribe('custom_event', (event) => {
+          if (event.data.eventName === 'Search') {
+            fbq('track', 'Search', {
+              content_ids: [event.data.searchQuery],
+            });
+          }
+        });
+      }
+    } else {
+      console.error('Facebook Pixel not initialized.');
     }
 
-    // Clean up (optional)
+    // Optional Cleanup: Consider whether you need to remove fbq
     return () => {
-      delete window.fbq;
+      if (window.fbq) {
+        window.fbq.queue = [];
+      }
     };
   }, []);
 
