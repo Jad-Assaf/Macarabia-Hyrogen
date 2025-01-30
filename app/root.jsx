eff73ba101e87580fb2bcb7f3c6bf1b9c915db10
Jad Assaf <jad.a.assaf@gmail.com>
2025-01-30 14:20:48 +0200

    Youtube Videos

--- app/root.jsx
+++ app/root.jsx
@@ -205,24 +205,20 @@ export function Layout({children}) {
         />
 
         <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3PZN80E9FJ"
-        ></script>
-        <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
+              function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-3PZN80E9FJ', { 'debug_mode': true });
-              
-              // Fire a test pageview event
-              gtag('event', 'page_view', { 
-                page_path: window.location.pathname
-              });
+
+              gtag('config', 'G-3PZN80E9FJ');
             `,
           }}
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3PZN80E9FJ"
+        ></script>
         <Suspense fallback={null}>
           <MetaPixel pixelId={PIXEL_ID} />
         </Suspense>
