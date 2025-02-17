6344aed72b0b7e10f62e565bbf9143043fcdb387
Jad Assaf <jad.a.assaf@gmail.com>
2025-02-17 11:28:29 +0200

    Conversions API Integration

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -339,7 +339,7 @@ export function ProductForm({
     trackAddToCartGA(product);
 
     // Trigger server‑side CAPI event
-    fetch('/api/track-facebook-event', {
+    fetch('/lib/track-facebook-event', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
