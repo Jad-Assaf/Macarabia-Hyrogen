b5af609e3eba6d12ed7f856fac5b53699fab9e6d
Jad Assaf <jad.a.assaf@gmail.com>
2025-02-18 11:10:10 +0200

    Conversions API Integration

--- app/root.jsx
+++ app/root.jsx
@@ -21,7 +21,7 @@ import {PageLayout} from '~/components/PageLayout';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 import React, {Suspense, useEffect, useState} from 'react';
 import ClarityTracker from './components/ClarityTracker';
-const MetaPixel = React.lazy(() => import('./components/MetaPixel'));
+import MetaPixel from './components/MetaPixel';
 
 /**
  * This is important to avoid re-fetching root queries on sub-navigations
@@ -207,9 +207,7 @@ export function Layout({children}) {
             `,
           }}
         ></script>
-        <Suspense fallback={null}>
-          <MetaPixel pixelId={PIXEL_ID} />
-        </Suspense>
+        <MetaPixel pixelId={PIXEL_ID} />
       </head>
       <body>
         <ClarityTracker clarityId={clarityId} />
