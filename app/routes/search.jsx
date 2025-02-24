16237a510caf02bd38f87368643206d17f8d3e10
Jad Assaf <jad.a.assaf@gmail.com>
2025-02-24 11:13:04 +0200

    Search

--- app/routes/search.jsx
+++ app/routes/search.jsx
@@ -971,7 +971,7 @@ async function predictiveSearch({request, context, usePrefix}) {
     // then OR them together
     const orSynonyms = synonyms.map((syn) => {
       const termWithWildcard = usePrefix ? `${syn}*` : `*${syn}*`;
-      return `(variants.sku:${termWithWildcard} OR title:${termWithWildcard} OR description:${termWithWildcard})`;
+      return `(variants.sku:${termWithWildcard} OR title:${termWithWildcard} OR description:${termWithWildcard} OR product_type:${termWithWildcard})`;
     });
     // Wrap this single word's synonyms in parentheses and join with OR
     return `(${orSynonyms.join(' OR ')})`;
