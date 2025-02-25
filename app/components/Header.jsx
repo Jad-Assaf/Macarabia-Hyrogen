8e60e97309b613336f7dc9d56618ea45c79502c2
Jad Assaf <jad.a.assaf@gmail.com>
2025-02-25 14:00:13 +0200

    Search

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,7 +5,6 @@ import {Image} from '@shopify/hydrogen-react';
 import {SearchFormPredictive, SEARCH_ENDPOINT} from './SearchFormPredictive';
 import {SearchResultsPredictive} from '~/components/SearchResultsPredictive';
 import { trackSearch } from '~/lib/metaPixelEvents'; // Added: Import the trackSearch function
-import SearchaniseWidget from './SearchaniseWidget.client';
 
 export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
   const {shop, menu} = header;
@@ -173,6 +172,7 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
 
               return (
                 <>
+                  {/* Fullscreen Overlay */}
                   <div
                     className={`search-overlay ${
                       isOverlayVisible ? 'active' : ''
@@ -180,6 +180,7 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
                     onClick={handleCloseSearch}
                   ></div>
 
+                  {/* Main Search Form */}
                   <div ref={searchContainerRef} className="main-search">
                     <div className="search-container">
                       <input
@@ -276,7 +277,6 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
               );
             }}
           </SearchFormPredictive>
-          <SearchaniseWidget />
 
           <div className="header-ctas">
             <NavLink
