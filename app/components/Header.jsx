8de2bf5030a5c3e1c42029538d7daa118c28536a
Jad Assaf <jad.a.assaf@gmail.com>
2025-02-25 14:15:01 +0200

    Search

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,7 +5,6 @@ import {Image} from '@shopify/hydrogen-react';
 import {SearchFormPredictive, SEARCH_ENDPOINT} from './SearchFormPredictive';
 import {SearchResultsPredictive} from '~/components/SearchResultsPredictive';
 import { trackSearch } from '~/lib/metaPixelEvents'; // Added: Import the trackSearch function
-import SearchaniseWidget from './SearchaniseWidget.client';
 
 export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
   const {shop, menu} = header;
@@ -105,8 +104,6 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
               height="79px"
             />
           </NavLink>
-
-          <SearchaniseWidget />
 
           <SearchFormPredictive className="header-search">
             {({inputRef, fetchResults, goToSearch, fetcher}) => {
