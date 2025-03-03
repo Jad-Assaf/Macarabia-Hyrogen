08639c9c45085156d7f95d64365ccf3241c894a2
Jad Assaf <jad.a.assaf@gmail.com>
2025-03-03 14:01:53 +0200

    Search

---
+++ app/routes/search-test.server.jsx
@@ -0,0 +1,7 @@
+// routes/search-test.server.jsx
+import React from 'react';
+import {Search} from '../components/Search.client';
+
+export default function SearchTestPage() {
+  return <Search />;
+}
